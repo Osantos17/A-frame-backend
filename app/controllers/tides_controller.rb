7d77033ce91ab261d0d3daa5a80ee944eb50655c
Orlando Santos <Santos.Orlando17@gmail.com>
2023-10-17 16:28:38 -0700

    init

--- app/controllers/tides_controller.rb
+++ app/controllers/tides_controller.rb
@@ -1,2 +1,11 @@
+require "http"
+
 class TidesController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    response = HTTP.get("http://api.worldweatheronline.com/premium/v1/marine.ashx?key=f1f00d06db194dd38e4190248232509&q=37.491711,-122.479787&format=json&tide=yes&tp=string")
+
+    render json: response.parse(:json)
+  end
 end
